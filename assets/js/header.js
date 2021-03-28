c7462aa6d2efbd600156a0b5119c91a0bee3cd5e
Riadh <rtalbi@nordsoft.fr>
2021-03-28 19:02:22 +0200

    supp header js

--- assets/js/header.js
+++
@@ -1,6 +0,0 @@
- $( document ).ready(function() {
-    $(".navbar-nav .nav-item").on("click", function(){
-        $(".nav-item").find(".active").removeClass("active");
-        $(this).addClass("active");
-    });
-});
