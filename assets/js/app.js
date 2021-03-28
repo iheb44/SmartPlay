41a7258280b042cb8765b7bc3dc370e041543e1a
Riadh <rtalbi@nordsoft.fr>
2021-03-28 18:54:43 +0200

    maj

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 /*import '../styles/app.css';*/
 import '../styles/global.scss';
+import "../styles/header.css";
+import "../styles/footer.css";
 
 // start the Stimulus application
 import '../bootstrap';
 const $ = require('jquery');
 require('bootstrap');
-import '../js/header.js';
+
